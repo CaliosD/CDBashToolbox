4cf03d074c615866ebff09e46a9f3a3de7b1061e
Calios <995967851@qq.com>
2016-12-30 18:04:52 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CDBashToolbox
-Some useful bash scripts.
+This repository is used to store some useful bash scripts. Written by myself or searched from elsewhere.
+
+# Description & Usage
+
+### auto\_pod
+
+`auto_pod` is used to create `Podfile` at given Xcode project path.
+
+```
+$ ./auto_pod ~/your/project/path
+```
+
+### clear\_node
+
+`clear_node` is used to delete node in your computer completely.
+
+### dock\_handler
+
+`dock_handler` is actually **not a executable shell script**, it's a bunch of commands with which you can custom your dock, such as change count of icons per column or per row, show only open apps in dock, etc.
--- auto_pod.sh
+++ auto_pod.sh
@@ -2,21 +2,31 @@
 
 clear
 
-find_proj='ls | grep -i .xcodeproj'
-proj_full_name="$(eval $find_proj)"
-proj_name="${proj_full_name%.*}"
+cd $1
 
-check_path='ls | grep -i .xcodeproj | wc -l'
-count="$(eval $check_path)"
-if [ "$count" -eq "1" ] 
-then
-	echo "##################################"
-	echo "platform:ios, '8.0'" > Podfile
-	echo "target '$proj_name' do" >> Podfile
-	echo "" >> Podfile
-	echo "end" >> Podfile
-	echo "Podfile is created."
-	echo "##################################"
+if [[ "$1" ]]; then
+	find_proj='ls | grep -i .xcodeproj'
+	proj_full_name="$(eval $find_proj)"
+	proj_name="${proj_full_name%.*}"
+
+	check_path='ls | grep -i .xcodeproj | wc -l'
+	count="$(eval $check_path)"
+	if [ "$count" -eq "1" ] 
+	then
+		echo "##################################"
+		echo "platform:ios, '8.0'" > Podfile
+		echo "target '$proj_name' do" >> Podfile
+		echo "" >> Podfile
+		echo "end" >> Podfile
+		echo "Podfile is created at path $1."
+		echo "##################################"
+	else
+		echo "You're not at the correct path."
+	fi
 else
-	echo "You're not at the correct path."
+	echo "Path for Xcode project is required."
 fi
+
+
+
+
